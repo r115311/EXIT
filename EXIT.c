0b2e4c38162df8995da1d5ccf6ce17cc54682465
Exit924 <43838335+Exit924@users.noreply.github.com>
2019-01-31 13:33:22 +0900

    Add files via upload

--- EXIT.c
+++ EXIT.c
@@ -36,6 +36,7 @@ int last[100];//前回の答案結果。正解していれば1初期状態0
 int total;//保存されている単語数
 int unuse;//初期状態0。一度でもといたことのあるファイルの場合１
 char filename[1][30];//ファイル名
+int success=0;//正解数
 
 //出力に関する関数宣言
 void errorp(char a[200]);//赤色
@@ -57,6 +58,7 @@ void debug();//変数内容確認（完成時削除予定
 void filewrite();//変数保存関数
 int filecheck();//ファイルの有無確認
 int fileread();//変数読み出し関数
+void ending();//プログラム終了時の動作
 
 
 
@@ -126,8 +128,12 @@ void mainmanu(){
 				strcpy(filename[0],"./Databases/L1-1");
 				gomi=fileread();
 				break;
+			case 9:
+				system("clear");
+				break;
 			case 0:
 				infop("Good Bye!!");
+				ending();
 				return;
 			default:
 				errorp("存在しないモードが選択されました。");
@@ -166,7 +172,30 @@ int fileread(){
 }
 
 void skim(){
-	infop("AraTsuBAの一覧表示させる関数");
+	int i,j,len;
+	char a[1][15];
+	for(;;){
+		questionp("表示したい問題のファイル名を入力してください。");
+		scanf("%s",a[0]);
+		len=strlen(a[0]);
+		if(len>15){
+			errorp("ファイル名が長すぎます!");
+			return;
+		}
+		strcpy(filename[0],"./Databases/");
+		strcat(filename[0],a[0]);
+		j=fileread();
+		if(j==0) break;
+	}
+	infop("---------------------------------------------------\n");
+	infop("|番号|失敗回数|    英語    |  日本語訳  |前回の結果|\n");
+	for(i=0;i<total;i++){
+		printf("%d,%d,%s,%s,%d\n",memorize[i],mistake[i],eng[i],jap[i],last[i]);
+	}
+	infop("---------------------------------------------------\n");
+	printf("総単語数：%d\n",total);
+	printf("unuse=%d\n",unuse);
+	okp("一覧出力完了.\n");
 }
 
 void words(){
@@ -261,11 +290,78 @@ int filecheck(){
 
 
 void solve(){
-	infop("はぬとりの問題を解く関数");
+	int i,j,rslt,len;
+	char ans[1][30];
+	char a[1][15];
+	for(;;){
+		printf("解きたい問題のファイル名を入力してください\n");
+		scanf("%s",a[0]);
+		len=strlen(a[0]);
+		if(len>15){
+			errorp("ファイル名が長過ぎます");
+			return;
+		}
+		strcpy(filename[0],"./Databases/");
+		strcat(filename[0],a[0]);
+		j=fileread();
+		if(j==0) break;
+	}
+	system("clear");
+	for(i=0;i<total;i++){
+		printf("%s\n→ ",jap[i]);
+		scanf("%s",ans[0]);
+		rslt=strcmp(eng[i],ans[0]);
+		if(rslt==0){
+			printf("正解！\n\n");
+			memorize[i]=1;
+			last[i]=1;
+			success++;
+		}else{
+			printf("不正解！(%s)\n\n",eng[i]);
+			mistake[i]++;
+			last[i]=0;
+		}
+	}
+	filewrite();		
 }
 
 void weak(){
-	infop("はぬとりの苦手関数");
+	int i,j,rslt,len;
+	char ans[1][30];
+	char a[1][15];
+	for(;;){
+		printf("解きたい問題のファイル名を入力してください\n");
+		scanf("%s",a[0]);
+		len=strlen(a[0]);
+		if(len>15){
+			errorp("ファイル名が長過ぎます");
+			return;
+		}
+		strcpy(filename[0],"./Databases/");
+		strcat(filename[0],a[0]);
+		j=fileread();
+		if(j==0) break;
+	}
+	system("clear");
+	for(i=0;i<total;i++){
+		if(memorize[i]==0){
+			printf("%s\n→ ",jap[i]);
+			scanf("%s",ans[0]);
+			rslt=strcmp(eng[i],ans[0]);
+			if(rslt==0){
+				printf("正解！\n\n");
+				memorize[i]=1;
+				last[i]=1;
+				success++;
+			}else{
+				printf("不正解！(%s)\n\n",eng[i]);
+				mistake[i]++;
+				last[i]=0;
+			}
+		}
+	}
+	filewrite();
+	
 }
 void skimn(){
 	int i;
@@ -365,6 +461,7 @@ void debug(){
 	infop("-----Debug Mode!!-----");
 	printf("単語数は%d\n",total);
 	printf("unuse=%d\n",unuse);
+	printf("memorize[i],mistake[i],jap[i],eng[i],last[i]\n");
 	for(i=0;i<total;i++){
 		printf("%d,%d,%s,%s,%d\n",memorize[i],mistake[i],jap[i],eng[i],last[i]);
 	}
@@ -392,5 +489,44 @@ void questionp(char a[200]){
 
 void optionp(char a[200]){
 	printf(BOLDWHITE"%s\n"RESET,a);
+}
+
+void ending(){
+	int data[12];
+	int q,max,trash;
+	char *plotfile="./gnu.csv";
+	
+	max=0;
+	FILE* fg=fopen(plotfile,"r");
+	if(fg!=NULL){
+		for(q=0;q<10;q++){
+			fscanf(fg,"%d %d",&trash,&data[q]);
+		}
+		fclose(fg);
+	}else{
+		if(success==0)return;
+		for(q=0;q<11;q++){
+			data[q]=0;
+		}
+	}
+	data[10]=success;
+	data[11]=0;
+	
+	FILE* FE=fopen(plotfile,"w");
+	for(q=1;q<12;q++){
+		fprintf(FE,"%d %d\n",q,data[q]);
+		if(data[q]>max){
+			max=data[q];
+		}
+	}
+	fclose(FE);
+	FILE* gp=popen("gnuplot -persist","w");
+	fprintf(gp,"set style fill solid border lc rgb \"black\"\n");
+	fprintf(gp,"set xrange[0:11]\n");
+		fprintf(gp,"set yrange[0:%d]\n",max+1);
+		fprintf(gp,"set xlabel 'oldest........................latest'\n");
+		fprintf(gp,"set ylabel 'memorized word'\n");
+		fprintf(gp,"plot \"gnu.csv\" using 1:2 with boxes lw 2 lc rgb \"light-cyan\" notitle\n");
+pclose(gp);
 }
 
